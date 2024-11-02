29dae44cbb08685a336a10ece25d739b53257b57
cemal can yıldırım <56302638+cemcois1@users.noreply.github.com>
2024-11-03 02:45:21 +0300

    Update Fonts.js

--- Components/CodeBase/Fonts/Fonts.js
+++ Components/CodeBase/Fonts/Fonts.js
@@ -2,10 +2,10 @@ import * as Font from 'expo-font';
 
 export const loadFonts = async () => {
     await Font.loadAsync({
+        'header': require('../Fonts/FontAssets/Lora-Regular.ttf'), // Header font
         'primary': require('../Fonts/FontAssets/Roboto-Regular.ttf') , // Primary font
         'primary-bold': require('../Fonts/FontAssets/Roboto-Bold.ttf'), // Primary bold font
         'secondary': require('../Fonts/FontAssets/OpenSans-Regular.ttf'), // Secondary font
         'secondary-bold': require('../Fonts/FontAssets/OpenSans-Bold.ttf'), // Secondary bold font
-        'header': require('../Fonts/FontAssets/Lora-Regular.ttf'), // Header font
     });
 };
